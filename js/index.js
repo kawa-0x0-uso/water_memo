59de58b6ce50c92568ec42c04b3ff3655601f1ab
kawa-0x0-uso <96374232+kawa-0x0-uso@users.noreply.github.com>
2023-07-20 16:57:36 +0900

    入力された値を配列に格納、合計を配列の合計で求めるように変更

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 let in_water = document.getElementById('input_water');
 let submit = document.getElementById('submit');
-let log_area = document.getElementById('log-area_list'); 
+let log_area = document.getElementById('log-area_list');
+
+let result_water = [];
 
 let total_score = 0;
 
+// 目標値
 const target = "2000" ;
 
 function input(){
     console.log("入力された値：" + in_water.value);
+
+    // 入力された値を配列に格納
+    result_water.push(parseInt(in_water.value));
+    console.log("今まで入力された値：" + result_water);
 
     // 履歴を入力
     let log_list = document.createElement('li');
@@ -15,12 +22,17 @@ function input(){
     log_area.appendChild(log_list);
 
     // 合計値を計算して表示
-    total_score = total_score + parseInt(in_water.value);
+    total_score = result_water.reduce(function(sum,element){
+        return sum + element;
+    },0);
+
     console.log("合計値：" + total_score);
 
     // 進捗率を計算して表示
+    let progress = total_score / target * 100;
+    console.log("進捗率：" + progress +"％");
 
-    // 前回の入力をクリアする
+    // 前回の入力値をクリアする
     in_water.value = "";
 }
 

700b680144cb2960595009fd743c62a571ee94b2
kawa-0x0-uso <96374232+kawa-0x0-uso@users.noreply.github.com>
2023-07-31 12:55:19 +0900

    ローカルストレージに保存と読み込みを実装

--- js/index.js
+++ js/index.js
@@ -2,19 +2,31 @@ let in_water = document.getElementById('input_water');
 let submit = document.getElementById('submit');
 let log_area = document.getElementById('log-area_list');
 
-let result_water = [];
+let input_water = new Array();
 
-let total_score = 0;
+// ローカルストレージオブジェクトを代入
+const storage = localStorage;
+
+let list = [];
 
 // 目標値
 const target = "2000" ;
 
 function input(){
+    // ローカルストレージからロードする
+    const json = storage.water_log;
+    if(json == undefined){
+        return;
+    }
+
+    list = JSON.parse(json);
+    console.log("ロード後：" + list);
+
     console.log("入力された値：" + in_water.value);
 
     // 入力された値を配列に格納
-    result_water.push(parseInt(in_water.value));
-    console.log("今まで入力された値：" + result_water);
+    input_water.push(parseInt(in_water.value));
+    console.log("今まで入力された値：" + input_water);
 
     // 履歴を入力
     let log_list = document.createElement('li');
@@ -22,7 +34,7 @@ function input(){
     log_area.appendChild(log_list);
 
     // 合計値を計算して表示する
-    total_score = result_water.reduce(function(sum,element){
+    let total_score = input_water.reduce(function(sum,element){
         return sum + element;
     },0);
 
@@ -32,18 +44,16 @@ function input(){
     let progress = total_score / target * 100;
     console.log("進捗率：" + progress +"％");
 
-    // 前回の入力値をクリアする
+    // 入力された値をクリアする
     in_water.value = "";
 
-    // ローカルストレージに配列を保存する
-    if (window.localStorage) {
-        let json = JSON.stringify(result_water, undefined, 1);
-        localStorage.setItem('result_log', json);
+    // ローカルストレージへ保存
+    list.push(input_water);
+    storage.water_log = JSON.stringify(list);
+    console.log("保存後：" + list);
+    localStorage.removeItem('water_log');
+    localStorage.removeItem('list');
 
-        json = localStorage.getItem('result_log');
-        result_water = JSON.parse(json);
-        console.log("ローカルストレージ：" + result_log);
-    }
 }
 
 // ボタン押下でイベント発火

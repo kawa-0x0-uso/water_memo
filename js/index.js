7a294db87f736f57484ba5e44f3ac1348c27beec
kawa-0x0-uso <96374232+kawa-0x0-uso@users.noreply.github.com>
2023-09-05 01:26:11 +0900

    ローカルストレージから値rのロード・保存・クリアを実装

--- js/index.js
+++ js/index.js
@@ -1,57 +1,46 @@
-let in_water = document.getElementById('input_water');
-let submit = document.getElementById('submit');
-let log_area = document.getElementById('log-area_list');
-
-let input_water = new Array();
-
 // ローカルストレージオブジェクトを代入
-const storage = localStorage;
+const STORAGE = localStorage;
+// ローカルストレージに保存されている値をロードする。
+let result = JSON.parse(STORAGE.getItem('waterLog'));
 
-let log = [];
+// 入力された値を取得するためのIDを取得
+let input = document.getElementById('input_water');
+// イベント発火用のボタンのIDを取得
+let submit = document.getElementById('submit');
+// ローカルストレージのクリアボタンのIDを取得
+let clear = document.getElementById('clear_btn');
+// 入力されたログを出力するためのエリアのIDを取得
+let log = document.getElementById('log-area_list');
 
 // 目標値
 const target = "2000" ;
 
-function input(){
-    // const json = storage.water_log;
-    // if(json == undefined){
-    //     return;
-    // }
+// ローカルストレージに値を保存する
+let saveData = (result) =>{
+    STORAGE.setItem('waterLog',JSON.stringify(result));
+}
 
-    // log = JSON.parse(json);
-    // console.log("ロード後：" + log);
+// ローカルストレージの値をクリアする
+let clearData = () =>{
+    // リサルトの値を初期化
+    result = [];
+    // 初期化したデータを上書き保存
+    STORAGE.setItem('waterLog',JSON.stringify(result));
+}
 
-    console.log("入力された値：" + in_water.value);
+// テキストエリアから入力された値を取得し、配列に格納する
+let getInput = () =>{
+    // 配列に入力された値を追加する
+    result.push(input_water.value);
+    console.log(result);
 
-    // 入力された値を配列に格納
-    input_water.push(parseInt(in_water.value));
-    console.log("今まで入力された値：" + input_water);
+    // 入力された値の初期化（テキストフォームから値を消す）
+    input_water.value = "";
 
-    // 履歴を入力
-    let log_list = document.createElement('li');
-    log_list.textContent = in_water.value + "ml" ;
-    log_area.appendChild(log_list);
-
-    // 合計値を計算して表示する
-    let total_score = input_water.reduce(function(sum,element){
-        return sum + element;
-    },0);
-
-    console.log("合計値：" + total_score);
-
-    // 進捗率を計算して表示する
-    let progress = total_score / target * 100;
-    console.log("進捗率：" + progress +"％");
-
-    // 入力された値をクリアする
-    in_water.value = "";
-
-    // ローカルストレージへ保存
-    const log = [];
-    log.push(input_water);
-    storage.water_log = JSON.stringify(log);
-    console.log("保存後：" + log);
+    // ローカルストレ―ジに保存
+    saveData(result);
 }
 
 // ボタン押下でイベント発火
-submit.addEventListener('click',input);
+submit.addEventListener('click',getInput);
+clear.addEventListener('click',clearData);
